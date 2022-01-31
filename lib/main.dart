409b0174e35e0e9aaf6c093542d5f3b22e9f82e6
Jossue <jossue2598@gmail.com>
2022-01-31 12:37:47 -0500

    listView Builder_Separated

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:componentes_app/screens/listview1_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:componentes_app/screens/listview2_screen.dart';
 
 void main() => runApp(const MyApp());
 
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Material App',
-      home: ListView1Screen(),
+      home: ListView2Screen(),
     );
   }
 }
