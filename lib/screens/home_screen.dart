278ca71839910bf71588b435d74f142f6931c841
Jossue <jossue2598@gmail.com>
2022-02-03 15:14:57 -0500

    diseñoHomeScreen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-   
   const HomeScreen({Key? key}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-         child: Text('HomeScreen'),
-      ),
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Componentes en FLutter'),
+          elevation: 0,
+        ),
+        body: ListView.separated(
+            itemBuilder: (context, index) => ListTile(
+                  title: const Text('Nombbre de lla ruta'),
+                  leading: const Icon(Icons.access_time_filled_sharp),
+                  onTap: () {},
+                ),
+            separatorBuilder: (context, index) => const Divider(),
+            itemCount: 10));
   }
 }
