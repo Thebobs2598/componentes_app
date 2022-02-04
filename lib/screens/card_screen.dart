c7e9fed420bc11cfd7c595ef187fb4d5d2ac2121
Jossue <jossue2598@gmail.com>
2022-02-03 21:55:41 -0500

    creandoCardScreen

--- lib/screens/card_screen.dart
+++ lib/screens/card_screen.dart
@@ -1,14 +1,30 @@
+import 'package:componentes_app/themes/themes.dart';
 import 'package:flutter/material.dart';
 
 class CardScreen extends StatelessWidget {
-   
   const CardScreen({Key? key}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-         child: Text('CardScreen'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Card Widget'),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 20,vertical: 10),
+        children: [
+          Card(
+            child: Column(
+              children: [
+                ListTile(
+                  leading: Icon(Icons.photo_album_outlined, color: AppDarkTheme.primary,),
+                  title: Text('Soy un titulo'),
+                  subtitle: Text('Eiusmod commodo anim duis dolor dolore .'),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
