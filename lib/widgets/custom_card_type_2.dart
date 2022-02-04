fd222042038601178e39c7c850ca664b6b5400bd
Jossue <jossue2598@gmail.com>
2022-02-04 09:07:22 -0500

    enviandoArgumentosWidgets

--- lib/widgets/custom_card_type_2.dart
+++ lib/widgets/custom_card_type_2.dart
@@ -1,9 +1,15 @@
-import 'package:componentes_app/themes/app_dark_theme.dart';
 import 'package:flutter/material.dart';
 
+import 'package:componentes_app/themes/themes.dart';
+
 class CustomCardType2 extends StatelessWidget {
+  final String imageUrl;
+  final String? name;
+
   const CustomCardType2({
     Key? key,
+    required this.imageUrl,
+    this.name,
   }) : super(key: key);
 
   @override
@@ -17,23 +23,24 @@ class CustomCardType2 extends StatelessWidget {
       elevation: 10,
       child: Column(
         children: [
-          const FadeInImage(
-            placeholder: AssetImage(
+          FadeInImage(
+            placeholder: const AssetImage(
               'assets/jar-loading.gif',
             ),
             image: NetworkImage(
-              'https://www.ninoversace.com/wp-content/uploads/landscape-mountains-sky-4843193.jpg',
+              imageUrl,
             ),
             width: double.infinity,
             height: 230,
             fit: BoxFit.cover,
-            fadeInDuration: Duration(milliseconds: 300),
+            fadeInDuration: const Duration(milliseconds: 300),
           ),
-          Container(
-            alignment: AlignmentDirectional.centerEnd,
-            padding: const EdgeInsets.only(right: 20, top: 10, bottom: 10),
-            child: const Text('Un hermoso paisaje'),
-          )
+          if (name!= null)
+            Container(
+              alignment: AlignmentDirectional.centerEnd,
+              padding: const EdgeInsets.only(right: 20, top: 10, bottom: 10),
+              child:  Text(name!),
+            )
         ],
       ),
     );
