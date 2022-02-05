c4d5895b4968e38b6e8ade90a4a04d21a17e0ff8
Jossue <jossue2598@gmail.com>
2022-02-05 11:03:07 -0500

    creandoAlertScreen

--- lib/themes/app_dark_theme.dart
+++ lib/themes/app_dark_theme.dart
@@ -4,17 +4,31 @@ class AppDarkTheme {
   static Color primary = Colors.indigo.shade700;
 
   static final ThemeData darkTheme = ThemeData.dark().copyWith(
-      //Color primario
-      primaryColor: primary,
+    //Color primario
+    primaryColor: primary,
 
-      //AppBar Theme
-      appBarTheme: AppBarTheme(
-        color: primary,
+    //AppBar Theme
+    appBarTheme: AppBarTheme(
+      color: primary,
+      elevation: 0,
+    ),
+
+    //TextButtonTheme
+    textButtonTheme: TextButtonThemeData(
+      style: TextButton.styleFrom(primary: primary),
+    ),
+
+    //FloatingActionButton
+    floatingActionButtonTheme: FloatingActionButtonThemeData(
+      backgroundColor: primary,
+      elevation: 5,
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        primary: Colors.indigo,
+        shape: const StadiumBorder(),
         elevation: 0,
-      ),
-
-      //TextButtonTheme
-      textButtonTheme: TextButtonThemeData(
-        style: TextButton.styleFrom(primary: primary),
-      ));
+      )
+    ),
+  );
 }
